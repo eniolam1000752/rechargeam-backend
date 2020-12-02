49a5a28a0f5fca5ed4fc1b0ae8e4d34ea3e80bd7
unknown <eniolam1000752@gmail.com>
2020-12-03 00:21:11 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appLogger } from './logger/logger.service';
+import 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(appLogger));
 
-  await app.listen(3500);
+  await app.listen(process.env.NODE_ENV !== 'production' ? 3500 : 9000);
 }
 bootstrap();
