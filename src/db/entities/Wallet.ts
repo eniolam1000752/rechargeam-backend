b44dd936a2d48351096d45d3a9fbf1676ecbc050
unknown <eniolam1000752@gmail.com>
2021-03-17 14:30:08 +0100

    added wallet entity implementation

---
+++ src/db/entities/Wallet.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { AdminUser } from './AdminUserEntity';
+import { Customer } from './CustomerEntity';
+import { Devices } from './DevicesEntity';
+import { UssdSchema } from './UssdSchemaEntity';
+
+@Entity()
+export class Wallet {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 0 })
+  amount: number;
+
+  @Column({ default: 0 })
+  commission: number;
+
+  @OneToOne(() => Customer, (cust) => cust.wallet)
+  @JoinColumn()
+  customer: Customer;
+
+  @Column({ type: 'timestamp', nullable: true, default: null })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', nullable: true, default: null })
+  updatedAt: Date;
+}
